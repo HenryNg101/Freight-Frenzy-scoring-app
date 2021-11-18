4fb4bab76ef243b25bca7a0f66b5d400c332497e
Nguyen <nguqy034@MLF122-30.local>
2021-11-18 14:20:43 +1030

    Add basic screens

---
+++ Assignment 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment 2
+//
+//  Created by Nguyen, Quoc Hung - nguqy034 on 18/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
