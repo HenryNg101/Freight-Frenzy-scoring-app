af320e803d8082d34b1ef07cebd15262fa0e9b3a
Nguyen <nguqy034@MLF122-30.local>
2021-11-18 22:32:23 +1030

    Adding photo feature

--- Assignment 2/DataManager.swift
+++ Assignment 2/DataManager.swift
@@ -41,62 +41,21 @@ class DataManager: NSObject {
     var end_parking_warehouse: Bool = false
     var end_completely_in_warehouse: Bool = false
     
-    /*
-    var scores: Int {
-        get {return score}
-        set(newScore){
-            score = newScore
-        }
-    }
-    
-    //Setter and getter for stage 1
-    var stage1_check: [Bool] {
-        get {return [duck_delivered, parking_alliance_storage_unit, parking_storage_unit, parking_warehouse, completely_in_warehouse, freight_storage_unit, freight_shipping_hub, duck_used, team_scoring_elem_used]}
-        set(newVal){
-            duck_delivered = newVal[0]
-            parking_alliance_storage_unit = newVal[1]
-            parking_storage_unit = newVal[2]
-            parking_warehouse = newVal[3]
-            completely_in_warehouse = newVal[4]
-            freight_storage_unit = newVal[5]
-            freight_shipping_hub = newVal[6]
-            duck_used = newVal[7]
-            team_scoring_elem_used = newVal[8]
-        }
-    }
-    
-    //Setter and getter for stage 2
-    var stage2_val: [Int] {
-        get {return [freight, freight_l1, freight_l2, freight_l3, shared_hub_freight]}
-        set(newVal){
-            freight = newVal[0]
-            freight_l1 = newVal[1]
-            freight_l2 = newVal[2]
-            freight_l3 = newVal[3]
-            shared_hub_freight = newVal[4]
-        }
-    }
-    
-    //Setter and getter for stage 3
-    var stage3_val: Int {
-        get {return ducks_delivered}
-        set(newVal){
-            ducks_delivered = newVal
-        }
-    }
-    
-    var stage3_check: [Bool] {
-        get {return [shared_hub_tipped, shipping_hub_balanced, shipping_hub_capped, end_parking_warehouse, end_completely_in_warehouse]}
-        set(newVal){
-            shared_hub_tipped = newVal[0]
-            shipping_hub_balanced = newVal[1]
-            shipping_hub_capped = newVal[2]
-            end_parking_warehouse = newVal[3]
-            end_completely_in_warehouse = newVal[4]
-        }
-    }*/
-    
     override init() {
         super.init()
+        
+        let url = NSURL(string: "http://www.partiklezoo.com/freightfrenzy/?")
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config)
+        let task = session.dataTask(with: url! as URL, completionHandler:
+            {(data, response, error) in
+            if (error != nil) { return; }
+            if let json = try? JSON(data: data!) {
+                for count in 0...json.count - 1 {
+                    print(json[count]["score"].string!)
+                }
+            }
+        })
+        task.resume()
     }
 }
---
+++ Assignment 2/RegisterViewController.swift
@@ -0,0 +1,50 @@
+//
+//  RegisterViewController.swift
+//  Assignment 2
+//
+//  Created by Nguyen, Quoc Hung - nguqy034 on 18/11/21.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func OpenCameraRoll(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.photoLibrary) {
+            let imagePicker = UIImagePickerController()
+            
+            imagePicker.delegate = self
+            imagePicker.sourceType = .photoLibrary
+            imagePicker.allowsEditing = false
+            
+            present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : AnyObject]) {
+        imageView.image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
