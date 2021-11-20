d4befb53d922966833f5f22c7d9051220bcd0466
Nguyen <nguqy034@MLF122-30.local>
2021-11-20 15:19:27 +1030

    Added high scores

---
+++ Assignment 2/HighScoreTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HighScoreTableViewCell.swift
+//  Assignment 2
+//
+//  Created by Nguyen, Quoc Hung - nguqy034 on 20/11/21.
+//
+
+import UIKit
+
+class HighScoreTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
